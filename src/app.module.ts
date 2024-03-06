a488d0249a5c40a2c64f6e2079a01236208f67fc
NikFreeman <Classic76@tut.by>
2024-03-06 07:35:00 +0300

    fix: saving files before refreshing line endings

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,7 +3,16 @@ import { ConfigModule } from '@nestjs/config';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { CatService } from './cat/cat.service';
+import { UsersController } from './users/users.controller';
+import { TracksController } from './tracks/tracks.controller';
+import { AlbumsController } from './albums/albums.controller';
+import { ArtistsController } from './artists/artists.controller';
+import { FavoritesController } from './favorites/favorites.controller';
+import { FavoritesService } from './favorites/favorites.service';
+import { ArtistsService } from './artists/artists.service';
+import { TracksService } from './tracks/tracks.service';
+import { AlbumsService } from './albums/albums.service';
+import { UsersService } from './users/users.service';
 
 @Module({
   imports: [
@@ -12,7 +21,21 @@ import { CatService } from './cat/cat.service';
       envFilePath: '.env',
     }),
   ],
-  controllers: [AppController],
-  providers: [AppService, CatService],
+  controllers: [
+    AppController,
+    UsersController,
+    TracksController,
+    AlbumsController,
+    ArtistsController,
+    FavoritesController,
+  ],
+  providers: [
+    AppService,
+    FavoritesService,
+    ArtistsService,
+    TracksService,
+    AlbumsService,
+    UsersService,
+  ],
 })
 export class AppModule {}
