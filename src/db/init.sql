ec2371e974567de81eddb6d07e9ff50a2e222edd
NikFreeman <Classic76@tut.by>
2024-03-19 13:14:14 +0300

    feat: add docker postgres

---
+++ src/db/init.sql
@@ -0,0 +1,14 @@
+-- Database: HLS-db
+
+-- DROP DATABASE IF EXISTS "HLS-db";
+
+CREATE DATABASE "HLS-db"
+    WITH
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'en_US.utf8'
+    LC_CTYPE = 'en_US.utf8'
+    LOCALE_PROVIDER = 'libc'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1
+    IS_TEMPLATE = False;
