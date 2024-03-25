e3f57f7f9d334116dd49ac2dd3de6986c8bf6042
NikFreeman <Classic76@tut.by>
2024-03-25 21:59:09 +0300

    feat: add service

--- src/main.ts
+++ src/main.ts
@@ -22,6 +22,7 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe());
 
   await app.listen(port);
+
   console.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
