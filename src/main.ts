a19917dfca4621faddea73ef4fe8140ac0e2b0fe
NikFreeman <Classic76@tut.by>
2024-04-01 23:30:30 +0300

    feat: add filter

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,19 @@
-import { NestFactory } from '@nestjs/core';
+import { HttpAdapterHost, NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { ValidationPipe } from '@nestjs/common';
+import { LoggingService } from './logging/logging.service';
+import { AllExceptionsFilter } from './helpers/all-exceptions.filter';
 // import metadata from './metadata';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
+  const httpAdapter = app.get(HttpAdapterHost);
+
+  const logger = await app.resolve(LoggingService);
+
   const port = configService.get<number>('PORT');
 
   const config = new DocumentBuilder()
@@ -19,10 +25,22 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
+  app.useLogger(logger);
   app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalFilters(new AllExceptionsFilter(httpAdapter, logger));
+
+  process.on('uncaughtException ', (reason) => {
+    logger.log('uncaughtException');
+    logger.error(reason);
+    throw reason;
+  });
+
+  process.on('unhandledRejection', (reason) => {
+    logger.log('unhandledRejection');
+    logger.error(reason);
+  });
 
   await app.listen(port);
-
-  console.log(`Application is running on: ${await app.getUrl()}`);
+  logger.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
