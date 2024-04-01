6df4ad7bf30d9db019a84e95c5c83cd421e27cbe
NikFreeman <Classic76@tut.by>
2024-04-01 23:29:07 +0300

    feat: add middleware

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 
 import { AppController } from './app.controller';
@@ -10,6 +10,8 @@ import { TracksModule } from './entities/tracks/tracks.module';
 import { FavoritesModule } from './entities/favorites/favorites.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { AuthModule } from './entities/auth/auth.module';
+import { LoggerModule } from './logging/logging.module';
+import { LoggerMiddleware } from './helpers/logger.middleware';
 
 @Module({
   imports: [
@@ -24,8 +26,13 @@ import { AuthModule } from './entities/auth/auth.module';
     FavoritesModule,
     PrismaModule,
     AuthModule,
+    LoggerModule,
   ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoggerMiddleware).forRoutes('*');
+  }
+}
---
+++ src/helpers/logger.middleware.ts
@@ -0,0 +1,23 @@
+import { LoggingService } from './../logging/logging.service';
+import { Injectable, NestMiddleware } from '@nestjs/common';
+import { NextFunction, Request, Response } from 'express';
+
+@Injectable()
+export class LoggerMiddleware implements NestMiddleware {
+  constructor(private readonly logger: LoggingService) {}
+  use(req: Request, res: Response, next: NextFunction) {
+    const { baseUrl, body, params } = req;
+    res.on('finish', () => {
+      const { statusCode } = res;
+
+      this.logger.log(
+        `Request: URL: ${baseUrl} \t PARAMS: ${JSON.stringify(
+          params,
+        )} \t BODY: ${JSON.stringify(body)}`,
+      );
+      this.logger.log(`Response: statusCode ${statusCode}`);
+    });
+
+    next();
+  }
+}
